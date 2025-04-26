a3676c620e126d597c026141ef284bd5882db76b
Anderson Larios <lariostito05@gmail.com>
2025-04-26 18:14:09 -0500

    Subir trabajo

---
+++ src/Ejercicio/Estudiante.java
@@ -0,0 +1,37 @@
+package Ejercicio;
+import java.util.Scanner;
+
+
+public class Estudiante {
+
+	private String nombre;
+    private int edad;
+    private double[] notas;
+    private double notaFinal;
+
+    public Estudiante(String nombre, int edad, int cantidadNotas) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.notas = new double[cantidadNotas];
+    }
+
+    public void ingresarNotas(Scanner sc) {
+        double suma = 0;
+        for (int i = 0; i < notas.length; i++) {
+            System.out.print("N" + (i + 1) + ": ");
+            notas[i] = sc.nextDouble();
+            suma += notas[i];
+        }
+        notaFinal = suma / notas.length;
+    }
+
+    public double getNotaFinal() {
+        return notaFinal;
+    }
+
+    public void mostrarDatos() {
+        System.out.printf("Nombre: %s, Edad: %d, promedio: %.2f\n", nombre, edad, notaFinal);
+    }
+}
+
+
---
+++ src/Ejercicio/main.java
@@ -0,0 +1,37 @@
+package Ejercicio;
+import java.util.Scanner;
+
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+        System.out.print("¿Cuántos estudiantes desea registrar? ");
+        int nEstudiantes = sc.nextInt();
+        System.out.print("¿Cuántas notas por estudiantes? ");
+        int nNotas = sc.nextInt();
+
+        Estudiante[] estudiantes = new Estudiante[nEstudiantes];
+
+        for (int i = 0; i < nEstudiantes; i++) {
+            System.out.println("Estudiante " + (i + 1) + ":");
+            System.out.print("Nombre: ");
+            sc.nextLine(); 
+            String nombre = sc.nextLine();
+            System.out.print("Edad: ");
+            int edad = sc.nextInt();
+
+            estudiantes[i] = new Estudiante(nombre, edad, nNotas);
+            estudiantes[i].ingresarNotas(sc);
+        }
+
+        System.out.println("----Estudiantes aprobados( promedio >14)----");
+        for (Estudiante e : estudiantes) {
+            if (e.getNotaFinal() >= 14) {
+                e.mostrarDatos();
+            }
+        }	
+	}
+
+}
